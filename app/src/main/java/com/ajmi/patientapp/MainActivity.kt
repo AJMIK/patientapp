4abcc471e7ab269527b3c4e61901b48509bbbdc7
AJMIK <ajmimkabeer24@gmail.com>
2020-12-12 15:05:58 +0530

    code changed

--- app/src/main/java/com/ajmi/patientapp/MainActivity.kt
+++ app/src/main/java/com/ajmi/patientapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.ajmi.patientapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun patientdetails(view: View) {
+        var getPname=findViewById<EditText>(R.id.pname)
+        var getDctr=findViewById<EditText>(R.id.dctr)
+        var getSym=findViewById<EditText>(R.id.sym)
+        Toast.makeText(this,getPname.text.toString(),Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,getDctr.text.toString(),Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,getSym.text.toString(),Toast.LENGTH_SHORT).show()
     }
 }
